daa278a90d38ff8662ff7cfe9b31e829688b34da
mattlusty123 <42315111+mattlusty123@users.noreply.github.com>
2019-07-15 12:47:15 +0100

    Create globals.js

---
+++ globals.js
@@ -0,0 +1,9 @@
+var app = SpreadsheetApp;
+var spreadsheet = app.getActiveSpreadsheet();
+var sheet = app.getActiveSheet();
+
+var ui = app.getUi();
+var sheetLastCol = sheet.getLastColumn();
+var log = Logger.log
+var range = sheet.getActiveRange();
+
