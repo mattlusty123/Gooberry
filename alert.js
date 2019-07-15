4bcc0439669d9eb7530d0ca1bfa4e0d0635c3207
mattlusty123 <42315111+mattlusty123@users.noreply.github.com>
2019-07-15 12:34:21 +0100

    Update Alert
    added flash, showObject, Object.entries

--- alert.js
+++ alert.js
@@ -3,6 +3,34 @@ function alert(s){
     ui.alert(s);
 }
 
-function props() {
-  alert("vector:\n mag: " + vector.mag + "\n dir: " + vector.dir);
+function flash(range) {
+  var bg = range.getBackgrounds();
+  range.setBackground(red);
+  app.flush();
+  Utilities.sleep(300);
+  range.setBackgrounds(bg);
+}
+
+function showObject(o) {
+  var keyVals = Object.entries(o);
+  var string = "";
+  for(var i = 0 ; i < keyVals.length; i++){
+    string = string + keyVals[i][0] + ": " + keyVals[i][1] + "\n"
+  }
+  return string;
+}
+
+// POLYFILL
+// Object.entries(obj)----- UNSUPPORTED (ECMAScript 3 - Javascript 1.6 - Google App Script)
+
+if (!Object.entries) {
+  Object.entries = function( obj ){
+    var ownProps = Object.keys( obj ),
+        i = ownProps.length,
+        resArray = new Array(i); // preallocate the Array
+    while (i--)
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    
+    return resArray;
+  };
 }
