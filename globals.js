1501ab8a40010c6c4f6708b000687def049994ad
mattlusty123 <42315111+mattlusty123@users.noreply.github.com>
2019-07-16 17:21:33 +0100

    Delete globals.js

--- globals.js
+++
@@ -1,9 +0,0 @@
-var app = SpreadsheetApp;
-var spreadsheet = app.getActiveSpreadsheet();
-var sheet = app.getActiveSheet();
-
-var ui = app.getUi();
-var sheetLastCol = sheet.getLastColumn();
-var log = Logger.log
-var range = sheet.getActiveRange();
-
